35af31b23135380b2a518c4ad14f94dcfb519efc
yocobra21 <artur.zayczev18@gmail.com>
2021-01-17 10:11:29 +0500

    done

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import { Select } from '/select/select';
 import './select/styles.scss';
 
-const select = new Select('#select', { classNames: 'select' });
+const select = new Select('#select', {
+  placeholder: 'take element',
+  selectedId: '2',
+  data: [
+    { id: '1', value: 'React' },
+    { id: '2', value: 'Vue' },
+    { id: '3', value: 'Nodejs' },
+    { id: '4', value: 'Next' },
+  ],
+  onSelect(item) {
+    console.log(item);
+  },
+});
 
 window.s = select;
--- select/select.js
+++ select/select.js
@@ -1,18 +1,27 @@
-const getTemplate = () => {
+const getTemplate = (data = [], placeholder, selectedId) => {
+  let text = placeholder ?? 'default';
+
+  const items = data.map((item) => {
+    let cls = '';
+    if (item.id === selectedId) {
+      text = item.value;
+      cls = 'selected';
+    }
+    return `
+    <li class="select__item ${cls}" data-type="item" data-id="${item.id}">${item.value}</li>
+    `;
+  });
+
   return `
+        <div class="select__backdrop" data-type="backdrop"></div>
         <div class="select__input" data-type="input">
-            <span>Text</span>
+            <span data-type="value">${text}</span>
             <i class="fas fa-chevron-down" data-type="arrow"></i>
         </div>
         <div class="select__dropdown">
             <ul class="select__list">
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
+                ${items.join('')}
             </ul>
-
         </div>
     `;
 };
@@ -20,19 +29,23 @@ const getTemplate = () => {
 export class Select {
   constructor(selector, options) {
     this.el = document.querySelector(selector);
+    this.options = options;
+    this.selectedId = options.selectedId;
     this.#render();
     this.#setup();
   }
 
   #render() {
+    const { placeholder, data } = this.options;
     this.el.classList.add('select');
-    this.el.innerHTML = getTemplate();
+    this.el.innerHTML = getTemplate(data, placeholder, this.selectedId);
   }
 
   #setup() {
     this.clickHandle = this.clickHandle.bind(this);
     this.el.addEventListener('click', this.clickHandle);
     this.arrow = this.el.querySelector('[data-type="arrow"');
+    this.value = this.el.querySelector('[data-type="value"');
   }
 
   clickHandle(event) {
@@ -40,11 +53,37 @@ export class Select {
 
     if (type === 'input') {
       this.toggle();
+    } else if (type === 'item') {
+      const id = event.target.dataset.id;
+      this.removeClassNameSelected(id);
+      this.select(id);
+    } else if (type === 'backdrop') {
+      this.close();
+    }
+  }
+
+  removeClassNameSelected(id) {
+    if (id != this.selectedId && this.selectedId != null) {
+      this.el
+        .querySelector(`[data-id="${this.selectedId}"]`)
+        .classList.remove('selected');
     }
   }
 
   get isOpen() {
     return this.el.classList.contains('open');
+  }
+
+  get current() {
+    return this.options.data.find((item) => item.id === this.selectedId);
+  }
+
+  select(id) {
+    this.selectedId = id;
+    this.value.textContent = this.current.value;
+    this.el.querySelector(`[data-id="${id}"]`).classList.add('selected');
+    this.options.onSelect ? this.options.onSelect(this.current) : null;
+    this.close();
   }
 
   toggle() {
@@ -65,5 +104,6 @@ export class Select {
 
   destroy() {
     this.el.removeEventListener('click', this.clickHandle);
+    this.el.innerHTML = '';
   }
 }
